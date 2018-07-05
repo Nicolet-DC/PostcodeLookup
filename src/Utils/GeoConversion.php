5f1f73e5dd527127ae7a0f789825e781ac5afff4
Tom Nicolet <tom.nicolet89@gmail.com>
2018-07-05 23:27:31 +0100

    Added php-coord for GeoConversion

---
+++ src/Utils/GeoConversion.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Utils;
+
+abstract class GeoConversion
+{
+    public static function toLatitude(int $easting):float
+    {
+
+    }
+    public static function toLongitude(int $northing):float
+    {
+
+    }
+}
