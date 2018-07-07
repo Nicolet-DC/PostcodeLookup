f77a726bac8d95316c50ba197923b17468b7740e
Tom Nicolet <tom.nicolet89@gmail.com>
2018-07-07 16:51:46 +0100

    Created a conversion utility for OS coordinate conversion.

--- src/Utils/GeoConversion.php
+++ src/Utils/GeoConversion.php
@@ -2,14 +2,27 @@
 
 namespace App\Utils;
 
+use PHPCoord\LatLng;
+use PHPCoord\OSRef;
+
 abstract class GeoConversion
 {
+
+    public static function toLatLng(int $easting, int $northing):LatLng
+    {
+        $OSRef = new OSRef($easting, $northing);
+        return $OSRef->toLatLng();
+    }
+
     public static function toLatitude(int $easting):float
     {
-
+        $OSRef = new OSRef($easting, 0);
+        return $OSRef->getOriginLatitude();
     }
+
     public static function toLongitude(int $northing):float
     {
-
+        $OSRef = new OSRef(0, $northing);
+        return $OSRef->getOriginLongitude();
     }
 }
